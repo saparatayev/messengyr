1f017975a2eef73cee42c442fcb42a857cbec11f
saparatayev <sopa.97@mail.ru>
2023-07-01 19:30:39 +0500

    pass some data to view from controller

--- lib/messengyr_web/controllers/page_controller.ex
+++ lib/messengyr_web/controllers/page_controller.ex
@@ -4,7 +4,7 @@ defmodule MessengyrWeb.PageController do
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home, layout: false)
+    render(conn, :home, layout: false, my_name: "Trustan")
   end
 
   def say_hello(conn, _params) do
